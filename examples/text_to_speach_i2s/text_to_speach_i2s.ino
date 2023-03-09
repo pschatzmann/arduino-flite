5b54f4f03edbfec92b229045e26647456737feec
pschatzmann <phil.schatzmann@gmail.com>
2023-03-09 15:25:25 +0100

    I2S using audio-tools

--- examples/text_to_speach_i2s/text_to_speach_i2s.ino
+++ examples/text_to_speach_i2s/text_to_speach_i2s.ino
@@ -1,16 +1,26 @@
-#include "flite_arduino.h"
+#include "flite_arduino.h" // install https://github.com/pschatzmann/arduino-flite
+#include "AudioTools.h" // install https://github.com/pschatzmann/arduino-audio-tools
+//#include "AudioLibs/AudioKit.h"
 
-Flite flite(new FliteOutputI2S());
+I2SStream out; // Replace with desired class e.g. AudioKitStream, AnalogAudioStream etc.
+Flite flite(out);
 
 
 void setup(){
-    Serial.begin(115700);
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+
+  // start data sink
+  auto cfg = out.defaultConfig();
+  cfg.sample_rate = 8000;
+  cfg.channels = 1;
+  cfg.bits_per_sample = 16;
+  out.begin(cfg);
 }
 
-
-void loop(){
-    const char* hallo ="hallo";
-    Serial.println(hallo);
-    flite.say(hallo);
-    delay(1000);
+// Arduino loop  
+void loop() {  
+  Serial.println("providing data...");
+  flite.say("Hallo, my name is Alice,");
+  delay(5000);
 }
